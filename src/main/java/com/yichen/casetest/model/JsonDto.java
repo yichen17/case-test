5de25ce546537b1ef44251879d275bf27408aef9
qiuxinchao <qiuxinchao@fbank.com.cn>
2022-02-23 15:51:22 +0800

    json理解

--- src/main/java/com/yichen/casetest/model/JsonDto.java
+++ src/main/java/com/yichen/casetest/model/JsonDto.java
@@ -2,6 +2,8 @@ package com.yichen.casetest.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * @author Qiuxinchao
  * @version 1.0
@@ -13,4 +15,10 @@ public class JsonDto {
     private String name;
     private Integer age;
     private String address;
+    List<String> foods;
+
+    @Data
+    public static class Inner{
+        private String time = "2022-02-23";
+    }
 }
--- src/main/java/com/yichen/casetest/test/JsonTest.java
+++ src/main/java/com/yichen/casetest/test/JsonTest.java
@@ -2,6 +2,12 @@ package com.yichen.casetest.test;
 
 import com.alibaba.fastjson.JSON;
 import com.yichen.casetest.model.JsonDto;
+import org.apache.commons.compress.utils.Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Qiuxinchao
@@ -15,6 +21,10 @@ public class JsonTest {
         JsonDto dto=new JsonDto();
         dto.setAddress("一条街");
         dto.setName("");
+        List<String> food=new LinkedList<>();
+        food.add("红烧肉");
+        food.add("白菜豆腐");
+        dto.setFoods(food);
         System.out.println(JSON.toJSONString(dto));
     }
 }
