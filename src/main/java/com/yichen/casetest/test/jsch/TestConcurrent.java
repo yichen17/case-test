bbbbaf2cba5074cec600e88315c9973c0c334161
qiuxinchao <qiuxinchao@fbank.com.cn>
2022-05-13 09:20:02 +0800

    jsch 并发性研究

--- src/main/java/com/yichen/casetest/test/jsch/TestConcurrent.java
+++ src/main/java/com/yichen/casetest/test/jsch/TestConcurrent.java
@@ -7,21 +7,25 @@ import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
 import java.util.Collection;
+
 /**
  * @author Qiuxinchao
  * @version 1.0
- * @date 2022/4/28 15:11
+ * @date 2022/5/12 16:16
  * @describe
  */
-public class Test1 {
+public class TestConcurrent {
     public static void main(String[] args) throws JSchException, SftpException {
         Collection<File> files = FileUtils.listFiles(new File("F:\\data\\upload"), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
-        long start = System.currentTimeMillis();
         SftpUtil2 sftpUtil = new SftpUtil2("152.136.237.34", 22, "root", "5r:u{CK!MF*n3","UTF-8");
-        for (File file : files) {
-            sftpUtil.uploadFile(file.getPath(),"/test/upload");
-        }
-        System.out.println("上传完毕,耗时：" + (System.currentTimeMillis() - start));
+        files.parallelStream().forEach(file -> {
+            try {
+                sftpUtil.uploadFile(file.getPath(),"/test/upload");
+            } catch (SftpException | JSchException e) {
+                e.printStackTrace();
+            }
+        });
+
         sftpUtil.disconnectAll();
     }
 }
