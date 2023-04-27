ea20006ce26e100dceb6c3c4ffa5fbb2a73b7068
qxc <q07218396@163.com>
2023-04-27 18:18:58 +0800

    map test

--- src/main/java/com/yichen/casetest/test/basetype/MapTest.java
+++ src/main/java/com/yichen/casetest/test/basetype/MapTest.java
@@ -1,9 +1,12 @@
 package com.yichen.casetest.test.basetype;
 
-import java.util.Collections;
+import com.yichen.casetest.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Qiuxinchao
@@ -11,10 +14,38 @@ import java.util.stream.Collectors;
  * @date 2023/2/7 21:17
  * @describe map测试
  */
+@Slf4j
 public class MapTest {
 
     public static void main(String[] args) {
+//        treeMapTest();
+//        StringUtils.divisionLine();
+        currentHashMapTest();
+        StringUtils.divisionLine();
+    }
 
+    private static void currentHashMapTest(){
+        ConcurrentHashMap<String, Object> map = new ConcurrentHashMap();
+        fillMap(map);
+    }
+
+    private static void treeMapTest(){
+        TreeMap<String, Object> treeMap = new TreeMap<>();
+        fillMap(treeMap);
+    }
+
+    private static void fillMap(Map treeMap){
+        treeMap.put("tt", 1);
+        treeMap.put("tt1", 1);
+        treeMap.put("tt2", 1);
+        treeMap.put("tt3", 1);
+        treeMap.put("tt4", 1);
+        treeMap.put("tt5", 1);
+
+        treeMap.put("name", "yichen");
+        treeMap.put("age", 18);
+        treeMap.put("address", "shanghai");
+        treeMap.put("sex", "boy");
     }
 
     private static void testFunction(){
