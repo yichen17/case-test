4916d766edbdc682f49729af383ceb8f5addd059
qxc <q07218396@163.com>
2023-06-15 18:22:42 +0800

    URI 测试

--- src/main/java/com/yichen/casetest/test/basetype/URITest.java
+++ src/main/java/com/yichen/casetest/test/basetype/URITest.java
@@ -1,6 +1,7 @@
 package com.yichen.casetest.test.basetype;
 
 import com.alibaba.fastjson.JSON;
+import com.yichen.casetest.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.URI;
@@ -16,10 +17,22 @@ import java.net.URISyntaxException;
 public class URITest {
 
     public static void main(String[] args) {
-//        String s = "redis://user:password@example.com:6379";
+
+        urlFieldTest();
+        StringUtils.divisionLine();
+
+    }
+
+
+    private static void urlFieldTest(){
+        //        String s = "redis://user:password@example.com:6379";
         String s= "https://wxtest.9fbank.com/h5/#/billList?token=ef1503005bb401002e505dca00e04b498b4ae8e1c7573906";
         try {
             URI uri = new URI(s);
+            log.info("{} {}", uri.getAuthority(), uri.getHost());
+            log.warn("analysis uri {}", JSON.toJSONString(uri));
+            uri = new URI("http://192.168.0.1:7421/test/hello");
+            log.info("{} {}", uri.getAuthority(), uri.getHost());
             log.warn("analysis uri {}", JSON.toJSONString(uri));
         }
         catch (URISyntaxException e){
