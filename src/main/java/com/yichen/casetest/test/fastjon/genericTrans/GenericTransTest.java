ac59afb35b8ea06f7e0b3595f33efb412b58d05c
qxc <q07218396@163.com>
2023-07-25 09:34:22 +0800

    save

--- src/main/java/com/yichen/casetest/test/fastjon/genericTrans/GenericTransTest.java
+++ src/main/java/com/yichen/casetest/test/fastjon/genericTrans/GenericTransTest.java
@@ -9,28 +9,48 @@ import java.util.List;
 /**
  * @author Qiuxinchao
  * @date 2023/7/20 14:28
- * @describe
+ * @describe   string 泛型转换
+ *      https://blog.csdn.net/qq_42896721/article/details/105091807
+ *      https://blog.csdn.net/xdx_dili/article/details/125560036
+ *
+ *  资料
+ *      http://en.wikipedia.org/wiki/Generics_in_Java#Type_erasure
+ *      http://gafter.blogspot.com/2006/12/super-type-tokens.html
+ *
  */
 @Slf4j
 public class GenericTransTest {
     private static final String s = "{\"name\":\"yichen\",\"age\":\"18\",\"goods\":{\"goodsName\":\"海底两万里\",\"goodsPrice\":12.34,\"goodsWeight\":\"2kg\"}}";
     private static final String ss = "{\"name\":\"yichen\",\"age\":\"18\",\"goods\":{\"goodsName\":\"海底两万里\",\"goodsPrice\":12.34,\"goodsWeight\":\"2kg\"}}";
+    private static final String sss = "{\"goodsName\":\"海底两万里\",\"goodsPrice\":12.34,\"goodsWeight\":\"2kg\"}";
 
     public static void main(String[] args) {
         testDirect();
-        Man<Object> trans = trans(s);
-        Man<List<Man.GoodsInfo>> other = other(ss);
+//        Man<Object> trans = trans(s);
+//        Man<List<Man.GoodsInfo>> other = other(ss);
+//        Man.GoodsInfo trans1 = simpleTrans(sss);
+
+
+//        Man.GoodsInfo goodsInfo = FastJsonUtils.fromJson(sss, new TypeReference<Man.GoodsInfo>() {
+//        });
+
+
         log.info("999");
     }
 
 
     private static  <T> Man<List<T>> other(String s){
-        return FastJsonUtils.fromJson(s, new TypeReference<Man<List<T>>>() {
+        return FastJsonUtils.fromJson(s, new TypeReference<Man<List<T>>>(List.class) {
         });
     }
 
     private static  <T> Man<T> trans(String s){
         return FastJsonUtils.fromJson(s, new TypeReference<Man<T>>() {
+        });
+    }
+
+    private static  <T>  T simpleTrans(String rr){
+        return FastJsonUtils.fromJson(rr, new TypeReference<T>() {
         });
     }
 
