30829ec235075c776d7498e298f3a2cfac8caa13
yichen <q07218396@163.com>
2023-08-14 21:04:44 +0800

    正则提取json

--- src/main/java/com/yichen/casetest/test/regex/RegexTest.java
+++ src/main/java/com/yichen/casetest/test/regex/RegexTest.java
@@ -1,5 +1,7 @@
 package com.yichen.casetest.test.regex;
 
+import com.yichen.casetest.utils.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -13,14 +15,31 @@ import java.util.regex.Pattern;
  */
 public class RegexTest {
 
-    private static final Pattern QUERY_STRING_PATTERN = Pattern.compile("(?<!\\{)\\?");
 
     public static void main(String[] args) {
+        test();
+        StringUtils.divisionLine();
+        test1();
+    }
+
+    private static final Pattern jsonSelect = Pattern.compile("\"comparison_type\":\"(\\d+)\"");
+    private static void test1(){
+        String s = "{\"attack_result\":{\"result\":false,\"score\":7.045269E-5,\"threshold\":0.5},\"biz_no\":\"\",\"comparison_type\":\"0\",\"device_risk_info\":{\"device_info_level\":\"0\",\"device_info_tags\":{\"is_hook\":0,\"is_injection\":0,\"is_root\":0,\"is_virtual_environment\":0}},\"images\":{\"image_best\":\"\"},\"request_id\":\"1687862649,27d95008-9ee3-40ac-899a-c2ffd7acf826\",\"result_code\":1000,\"result_message\":\"SUCCESS\",\"time_used\":357,\"user_faced_time\":\"1687862648\",\"verification\":{\"idcard\":{\"confidence\":-3.8044045,\"thresholds\":{\"1e-3\":62.169,\"1e-4\":69.315,\"1e-5\":74.399,\"1e-6\":78.038}}},\"video_key\":\"oxqPlHzBb7aUQSX4y1sSYTYaOzhqb7Hy\"}";
+        Matcher matcher = jsonSelect.matcher(s);
+        while (matcher.find()){
+            System.out.println(matcher.group(1));
+        }
+    }
+
+    private static final Pattern QUERY_STRING_PATTERN = Pattern.compile("(?<!\\{)\\?");
+    private static final Pattern regex = Pattern.compile("(\\$\\{[^\\}]+})");
+
+    private static void test(){
         try{
             int pos=0;
             List<String> list = new ArrayList<>(10);
             //    [^//}]+  表示一个或多个 不是 } 的    中括号内 ^ 表示取反。
-            Pattern regex = Pattern.compile("(\\$\\{[^\\}]+})");
+
             Matcher m = regex.matcher("'${capitalType}'!='9F'&&${tenantId}==1001");
             while (m.find()) {
                 String res=m.group();
@@ -39,4 +58,5 @@ public class RegexTest {
             System.out.println(e.getCause());
         }
     }
+
 }
