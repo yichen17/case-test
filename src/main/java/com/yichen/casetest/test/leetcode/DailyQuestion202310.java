5bedf777ba2aeabe864532b67bad340a40561fa1
yichen17 <q07218396@163.com>
2023-10-23 08:11:45 +0800

    简简单单一次过，最优。

--- src/main/java/com/yichen/casetest/test/leetcode/DailyQuestion202310.java
+++ src/main/java/com/yichen/casetest/test/leetcode/DailyQuestion202310.java
@@ -70,6 +70,25 @@ public class DailyQuestion202310 {
         StringUtils.divisionLine();
         maxSatisfactionTest(dq);
         StringUtils.divisionLine("maxSatisfactionTest");
+        countSeniorsTest(dq);
+        StringUtils.divisionLine();
+    }
+
+    // 2678. 老人的数目
+
+    private static void countSeniorsTest(DailyQuestion202310 dq){
+        System.out.println(dq.countSeniors(new String[]{"7868190130M7522","5303914400F9211","9273338290F4010"}));
+        System.out.println(dq.countSeniors(new String[]{"1313579440F2036","2921522980M5644"}));
+    }
+
+    public int countSeniors(String[] details) {
+        int result = 0;
+        for (String detail : details){
+            if ((detail.charAt(11) == '6' && detail.charAt(12) > '0') || detail.charAt(11) > '6'){
+                result++;
+            }
+        }
+        return result;
     }
 
     // 1402. 做菜顺序
