d2aaeb138005d606451522a7026cc7312255237e
yichen17 <q07218396@163.com>
2023-10-24 17:17:30 +0800

    格式化工具类待验证

--- src/main/java/com/yichen/casetest/utils/StringUtils.java
+++ src/main/java/com/yichen/casetest/utils/StringUtils.java
@@ -598,7 +598,72 @@ public class StringUtils {
     }
 
 
+
+    /**
+     * @param resString 待格式化的json字符串
+     * @return 格式化完毕后的字符串
+     */
+    private static String responseFormat(String resString){
+
+        StringBuilder jsonForMatStr = new StringBuilder();
+        int level = 0;
+        //将字符串中的字符逐个按行输出
+        for(int index=0;index<resString.length();index++)
+        {
+            //获取s中的每个字符
+            char c = resString.charAt(index);
+
+            //level大于0并且jsonForMatStr中的最后一个字符为\n,jsonForMatStr加入\t
+            if (level > 0  && '\n' == jsonForMatStr.charAt(jsonForMatStr.length() - 1)) {
+                jsonForMatStr.append(getLevelStr(level));
+            }
+            //遇到"{"和"["要增加空格和换行，遇到"}"和"]"要减少空格，以对应，遇到","要换行
+            switch (c) {
+                case '{':
+                case '[':
+                    jsonForMatStr.append(c + "\n");
+                    level++;
+                    break;
+                case ',':
+                    jsonForMatStr.append(c + "\n");
+                    break;
+                case '}':
+                case ']':
+                    jsonForMatStr.append("\n");
+                    level--;
+                    jsonForMatStr.append(getLevelStr(level));
+                    jsonForMatStr.append(c);
+                    break;
+                default:
+                    jsonForMatStr.append(c);
+                    break;
+            }
+        }
+        return jsonForMatStr.toString();
+    }
+    /**
+     * @param level
+     * @return
+     * @throws
+     * @author lgh
+     * @date 2018/10/29-14:29
+     */
+    private static String getLevelStr(int level) {
+        StringBuilder levelStr = new StringBuilder();
+        for (int levelI = 0; levelI < level; levelI++) {
+            levelStr.append("\t");
+        }
+        return levelStr.toString();
+    }
+
+
     public static void main(String[] args) {
+
+        System.out.println("\t666");
+
+        String jsonStr = "{\"id\":\"1\",\"name\":\"a1\",\"obj\":{\"id\":11,\"name\":\"a11\",\"array\":[{\"id\":111,\"name\":\"a111\"},{\"id\":112,\"name\":\"a112\"}]}}";
+        System.out.println(responseFormat(jsonStr));
+
         Address address = Address.builder().name("上海").province("浙江省").city("上海市").build();
         TestJson testJson = TestJson.builder().age(18).address(FastJsonUtils.toJson(address)).build();
         System.out.println(FastJsonUtils.toJson(testJson));
