5b027f009fa477f8ae0b9b966d17152838ee92f1
yichen17 <q07218396@163.com>
2023-11-03 08:34:15 +0800

    简简单单，还欠债2题。。

--- src/main/java/com/yichen/casetest/test/leetcode/DailyQuestion202311.java
+++ src/main/java/com/yichen/casetest/test/leetcode/DailyQuestion202311.java
@@ -2,6 +2,9 @@ package com.yichen.casetest.test.leetcode;
 
 import com.yichen.casetest.utils.StringUtils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author banYu
  * @version 1.0
@@ -14,6 +17,55 @@ public class DailyQuestion202311 {
         DailyQuestion202311 dq = new DailyQuestion202311();
         countPointsTest(dq);
         StringUtils.divisionLine();
+    }
+
+    // 117. 填充每个节点的下一个右侧节点指针 II
+
+    public Node connect(Node root) {
+        if (root == null){
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            Node pre = null, next;
+            while (len > 0){
+                next = queue.poll();
+                if (next.left != null){
+                    queue.offer(next.left);
+                }
+                if (next.right != null){
+                    queue.offer(next.right);
+                }
+                if (pre != null){
+                    pre.next = next;
+                }
+                pre = next;
+                len--;
+            }
+        }
+        return root;
+    }
+
+    private static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
     }
 
 
