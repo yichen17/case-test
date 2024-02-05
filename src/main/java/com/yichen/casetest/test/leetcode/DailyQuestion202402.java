8b84748e7c7b81e723806dd039aa74a182f49085
yichen17 <q07218396@163.com>
2024-02-05 23:18:13 +0800

    错了两次，我是菜鸡

--- src/main/java/com/yichen/casetest/test/leetcode/DailyQuestion202402.java
+++ src/main/java/com/yichen/casetest/test/leetcode/DailyQuestion202402.java
@@ -2,6 +2,8 @@ package com.yichen.casetest.test.leetcode;
 
 import com.yichen.casetest.utils.StringUtils;
 
+import java.util.ArrayDeque;
+
 /**
  * @author banYu
  * @version 1.0
@@ -15,6 +17,8 @@ class DailyQuestion202402 {
         stoneGameVITest(dq);
         StringUtils.divisionLine();
         canWinNimTest(dq);
+        StringUtils.divisionLine();
+        maxResultTest(dq);
         StringUtils.divisionLine();
     }
 
@@ -31,6 +35,39 @@ class DailyQuestion202402 {
 
     public int stoneGameVI(int[] aliceValues, int[] bobValues) {
         return 0;
+    }
+
+    // 1696. 跳跃游戏 VI
+
+    private static void maxResultTest(DailyQuestion202402 dq){
+        // 198
+        System.out.println(dq.maxResult(new int[]{100,-1,-100,-1,100}, 2));
+        // 7
+        System.out.println(dq.maxResult(new int[]{1,-1,-2,4,-7,3}, 2));
+        // 17
+        System.out.println(dq.maxResult(new int[]{10,-5,-2,4,0,3}, 3));
+        // 0
+        System.out.println(dq.maxResult(new int[]{1,-5,-20,4,-1,3,-6,-3}, 2));
+        System.out.println(dq.maxResult(StringUtils.randomIntArray(1000, -9999, 9999), 200));
+    }
+
+    public int maxResult(int[] nums, int k) {
+        int len = nums.length;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int[] dp = new int[len];
+        for (int i=0; i<len; i++){
+            // 维持有效长度
+            if (!stack.isEmpty() && i - stack.peekFirst() > k){
+                stack.removeFirst();
+            }
+            dp[i] =  nums[i] + (stack.isEmpty() ? 0 : dp[stack.peekFirst()]);
+            // 维持递减
+            while (!stack.isEmpty() && dp[i] > dp[stack.peekLast()]){
+                stack.removeLast();
+            }
+            stack.addLast(i);
+        }
+        return dp[len-1];
     }
 
     // 292. Nim 游戏
